85229c219bd97c8d3ebe8b141812d330ac27f619
Sa'di <88400372+Saadi0101@users.noreply.github.com>
2021-08-20 16:52:19 +0500

    Add files via upload

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    
+    class Animel
+    {
+        public string name;
+        public int age;
+        public int height;
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animel dog = new Animel();
+            Animel cat = new Animel();
+
+
+            dog.name = "Dog";
+            dog.age = 2;
+            dog.height = 50;
+
+
+            cat.name = "Cat";
+            cat.age = 3;
+            cat.height = 30;
+
+            Console.WriteLine($"Name: {dog.name}; Age: {dog.age}; Height: {dog.height}");
+            Console.WriteLine($"Name: {cat.name}; Age: {cat.age}; Height: {cat.height}");
+
+        }
+    }
+}
